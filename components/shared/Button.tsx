dca41a6825ed6ef4ad189c4f84c4743d07f4a922
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-01-28 16:49:04 +0100

    Carousel UI Fixes

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -15,7 +15,7 @@ const Button = ({ buttonText, color, isOutlined, href, additionalClasses }: Butt
         <Link href={href}>
             <div className={`flex items-center gap-2 ${isOutlined && 'border-[1.4px] border-opacity-60'} ${color === 'dark' ? 'border-slate-900' : 'border-white'} px-4 py-[6px] group ${additionalClasses && additionalClasses}`}>
                 <p className={`${color === 'dark' ? 'text-black' : 'text-white'} font-medium lg:font-semibold text-[18px] md:text-[20px] font-body`}>{buttonText}</p>
-                <HiOutlineArrowNarrowRight className="text-primaryOrange transition-transform duration-150 group-hover:translate-x-1 stroke-[2px] w-9 h-5" />
+                <HiOutlineArrowNarrowRight className="text-primaryOrange transition-transform duration-150 group-hover:translate-x-1 stroke-[2px] w-8 h-5" />
             </div>
         </Link>
     );
--- components/shared/carousel/Carousel.tsx
+++ components/shared/carousel/Carousel.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { SlideModel } from '@/types/SlideModel';
 import Slide from './Slide';
 import Button from '../Button';
+import { HiOutlineArrowNarrowLeft, HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
 interface CarouselProps {
     items: SlideModel[];
@@ -16,7 +17,9 @@ const Carousel = ({ items }: CarouselProps) => {
     return (
         <div className="w-full my-5 md:w-fit md:max-w-[1240px] md:mx-auto flex flex-col">
             <div className='w-full flex justify-center items-center gap-4 md:px-4'>
-                <button onClick={goPrev} className="hidden md:flex items-center justify-center w-10 h-10 pb-1 bg-black text-white rounded-full">←</button>
+                <button onClick={goPrev} className="hidden md:flex items-center justify-center w-10 h-10 bg-black text-white rounded-full">
+                    <HiOutlineArrowNarrowLeft className="text-white transition-transform duration-150 group-hover:translate-x-1 stroke-[2px] w-8 h-5" />
+                </button>
                 <div className="overflow-hidden w-full max-w-[862px] mx-auto shadow-lg">
                     <div className="flex h-fit transition-transform duration-300" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
                         {items.map((item) => (
@@ -26,7 +29,9 @@ const Carousel = ({ items }: CarouselProps) => {
                         ))}
                     </div>
                 </div>
-                <button onClick={goNext} className="hidden md:flex items-center justify-center w-10 h-10 pb-1 bg-black text-white rounded-full">→</button>
+                <button onClick={goNext} className="hidden md:flex items-center justify-center w-10 h-10 bg-black text-white rounded-full">
+                    <HiOutlineArrowNarrowRight className="text-white transition-transform duration-150 group-hover:translate-x-1 stroke-[2px] w-8 h-5" />
+                </button>
             </div>
             {/**INDICATORS */}
             <div className='flex justify-between w-full max-w-screen-md mx-auto'>
@@ -40,11 +45,15 @@ const Carousel = ({ items }: CarouselProps) => {
                     ))}
                 </div>
                 <div className="flex md:hidden items-center gap-4 m-4 mr-0">
-                    <button onClick={goPrev} className="w-10 h-10 pb-1 bg-black text-white rounded-full">←</button>
-                    <button onClick={goNext} className="w-10 h-10 pb-1 bg-black text-white rounded-full">→</button>
+                    <button onClick={goPrev} className="flex items-center justify-center w-10 h-10 pb-1 bg-black text-white rounded-full">
+                        <HiOutlineArrowNarrowLeft className="text-white transition-transform duration-150 stroke-[2px] w-8 h-5 pt-1" />
+                    </button>
+                    <button onClick={goNext} className="flex items-center justify-center w-10 h-10 pb-1 bg-black text-white rounded-full">
+                        <HiOutlineArrowNarrowRight className="text-white transition-transform duration-150 stroke-[2px] w-8 h-5 pt-1" />
+                    </button>
                 </div>
                 <div className='hidden md:flex items-center m-4'>
-                    <Button buttonText='Discover Collection' isOutlined={false} color='dark' href='/collection'/>
+                    <Button buttonText='Discover Collection' isOutlined={false} color='dark' href='/collection' />
                 </div>
             </div>
         </div>
