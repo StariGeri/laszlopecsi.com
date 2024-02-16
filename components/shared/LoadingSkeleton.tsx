439fb084d576c7a0b46cb5dc53eafb8289422b12
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-02-16 22:59:01 +0100

    Loading skeleton for the collection page

--- components/shared/LoadingSkeleton.tsx
+++ components/shared/LoadingSkeleton.tsx
@@ -1,9 +1,17 @@
+import CardSkeleton from "../collection/loadingSkeleton/CardSkeleton";
 
 
 const LoadingSkeleton = () => {
 
     return (
-        <div></div>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 min-[500px]:gap-5 lg:gap-7 w-full max-w-[1240px] xl:mx-auto my-8 md:my-12 lg:my-24">
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+        </div>
     );
 };
 
