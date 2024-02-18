e5cd7d1341ea940ebe0e91d2a190a9c035f31a16
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-02-18 09:09:12 +0100

    [DPT-19] Handling if the dimensions are not set

--- components/collection/ArtCard.tsx
+++ components/collection/ArtCard.tsx
@@ -7,7 +7,12 @@ import AvailableText from './AvailableText';
 
 const ArtCard = ({ art }: { art: ArtModel }) => {
 
+    // handle the exictence of the z-axis
     const zAxis = `${art.dimensions.z ? `x ${art.dimensions.z}` : ''}`;
+
+    // handle the dimensions in case they are null
+    const isDimensionNull = art.dimensions.x === 0 || art.dimensions.y === 0;
+    const dimensionsText = isDimensionNull ? '' : `${art.dimensions.x} x ${art.dimensions.y} ${zAxis} cm`;
 
     return (
         <div className='w-full h-[300px] md:h-[390px] lg:h-[420px] xl:h-[470px] flex flex-col mx-auto shadow hover:shadow-lg transition-all duration-200'>
@@ -21,7 +26,7 @@ const ArtCard = ({ art }: { art: ArtModel }) => {
                 <h1 className='font-body font-semibold text-lg md:text-xl lg:text-[26px]'>{art.title}</h1>
                 <p className='italic font-body font-medium text-base md:text-lg lg:text-[20px]'>{art.year ? art.year : 'Unknown'}</p>
                 <div className='w-full flex items-center justify-end sm:justify-between mt-auto'>
-                    <p className='font-normal font-body text-sm md:text-base lg:text-xl hidden sm:flex'>{art.dimensions.x} x {art.dimensions.y} {zAxis}  cm</p>
+                    <p className='font-normal font-body text-sm md:text-base lg:text-xl hidden sm:flex'>{dimensionsText}</p>
                     <AvailableText isAvailable={art.isAvailable} />
                 </div>
             </div>
