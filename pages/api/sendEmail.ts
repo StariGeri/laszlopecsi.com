b684121a68c6862ad71cca6f6c324ea5ba66f42e
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-02-27 18:27:14 +0100

    setting up courier instead of nodemailer

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -2,38 +2,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 // 3rd party libraries
-import nodemailer from 'nodemailer';
 import sanitizeHtml from 'sanitize-html';
+import { CourierClient } from '@trycourier/courier';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('reached the sendEmail API');
-
-  const createEmailTemplate = (name: string, email: string, subject: string, message: string) => {
-    return `
-          <div style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin: 0; padding: 20px; color: #333;">
-            <div style="max-width: 600px; margin: auto; background: #ffffff; border: 1px solid #ddd; border-radius: 8px; overflow: hidden;">
-              <header style="background-color: #D4A373; color: white; padding: 10px 20px; text-align: center;">
-                <h1 style="margin: 0; font-size: 24px;">New Message Received</h1>
-              </header>
-              <div style="padding: 20px;">
-                <p style="font-size: 18px; margin: 0 0 10px 0;"><strong>From:</strong> ${name}</p>
-                <p style="font-size: 18px; margin: 0 0 10px 0;"><strong>Email:</strong> <a href="mailto:${email}" style="color: #D4A373;">${email}</a></p>
-                <p style="font-size: 18px; margin: 0 0 10px 0;"><strong>Subject:</strong> ${subject}</p>
-                <div style="background-color: #f8f8f8; padding: 15px; margin: 20px 0; border-left: 4px solid #D4A373;">
-                  <p style="font-size: 16px; margin: 0;"><strong>Message:</strong></p>
-                  <p style="font-size: 16px;">${message}</p>
-                </div>
-              </div>
-              <footer style="background-color: #f2f2f2; text-align: center; padding: 10px 20px;">
-                <p style="font-size: 14px; margin: 0;">This email was sent from laszlopecsi.com's contact form.</p>
-              </footer>
-            </div>
-          </div>
-        `;
-  };
-
   if (req.method === 'POST') {
-    console.log('inside the if statement!');
     const { name, email, subject, message } = req.body;
 
     const sanitizedName = sanitizeHtml(name);
@@ -41,39 +14,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const sanitizedSubject = sanitizeHtml(subject);
     const sanitizedMessage = sanitizeHtml(message);
 
-    // Configure nodemailer transport
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: process.env.NEXT_PUBLIC_EMAIL_USER,
-        pass: process.env.NEXT_PUBLIC_EMAIL_PASS,
-      },
-    });
+    const courier = new CourierClient({ authorizationToken: process.env.NEXT_PUBLIC_COURIER_API_KEY });
 
-    console.log('transporter: ', transporter);
-
-    const mailOptions = {
-      from: `${process.env.NEXT_PUBLIC_EMAIL_USER}`, // Sender address: your own email
-      to: `${process.env.NEXT_PUBLIC_EMAIL_USER}`, // List of receivers: your own email
-      replyTo: sanitizedEmail, // Reply-to address set to the user's email
-      subject: `Inquiry through website: ${sanitizedSubject}`, // Subject line
-      html: createEmailTemplate(sanitizedName, sanitizedEmail, sanitizedSubject, sanitizedMessage), // HTML body
-    };
-
-    console.log('mailOptions: ', mailOptions);
-
-    // Send the email
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log('error: ', error);
-        res.status(500).json({ error: error });
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.status(200).json({ message: 'Email sent successfully!' });
-      }
-    });
-    // Close the transporter
-    transporter.close();
+    try {
+      const { requestId } = await courier.send({
+        message: {
+          to: {
+            email: 'sgergostarosta@gmail.com',
+          },
+          template: 'Z85M4HS9MHMP58J906CK1QP5QEV9',
+          data: {
+            name: sanitizedName,
+            email: sanitizedEmail,
+            subject: sanitizedSubject,
+            message: sanitizedMessage,
+          },
+        },
+      });
+      res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+      console.error('Error sending email', error);
+      res.status(500).json({ message: 'Error sending email' });
+    }
   } else {
     // Handle any non-POST requests
     res.setHeader('Allow', ['POST']);
