bfd61abddbe88033e6824ba914a0062f969102a1
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-02-29 23:16:46 +0100

    [DPT-53] Creating the custom 404 page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from 'next/app'
 import Layout from './_layout'
 
 function App({ Component, pageProps }: AppProps) {
+
+  // check if we are on the 404 page
+  if (Component.name === 'Custom404') {
+    return <Component {...pageProps} />
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
