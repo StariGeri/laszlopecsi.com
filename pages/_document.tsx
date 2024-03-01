9cc6a29ba55661ce2ad5e377592d32b28dadf8de
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-03-01 23:54:46 +0100

    Adding Google Analytics Tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 
 export default function Document() {
   return (
@@ -10,6 +12,7 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <GoogleAnalytics gaId='G-EN8CW121T9'/>
     </Html>
   )
 }
